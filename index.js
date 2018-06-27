e32e79b4e295978e5f89dec2e5d7a2ef86b28de2
emil14 <emil.musician@gmail.com>
2018-06-27 19:09:09 +0300

    Add start script and Update app

--- index.js
+++ index.js
@@ -2,10 +2,7 @@ const Telegraf = require('telegraf')
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
-bot.start((ctx) => ctx.reply('Welcome'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.on('sticker', (ctx) => ctx.reply('👍'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-bot.hears(/buy/i, (ctx) => ctx.reply('Buy-buy'))
+bot.start(ctx => ctx.reply('Hello friend'))
+bot.on('message', ctx => console.log(ctx.message))
 
 bot.startPolling()
