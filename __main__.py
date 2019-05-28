daf4f6855a33a11cdb8959c109a146295e066e2c
emil14 <emil.musician@gmail.com>
2019-05-29 01:41:26 +0300

    Update main script

--- __main__.py
+++ __main__.py
@@ -2,6 +2,7 @@ import os
 import asyncio
 
 import aiohttp
+from aiohttp import ClientSession
 
 
 def main():
@@ -14,11 +15,11 @@ def main():
     def server(base_url):
         async def handle_req(req):
             print(await req.json())
-            return aiohttp.web.Response(status=200)
+            return web.Response(status=200)
 
-        app = aiohttp.web.Application()
-        app.add_routes([aiohttp.web.post(f'/{base_url}/api/v1', handle_req)])
-        aiohttp.web.run_app(app)
+        app = web.Application()
+        app.add_routes([web.post(f'/{base_url}/api/v1', handle_req)])
+        web.run_app(app)
 
     base_url = f'https://api.telegram.org/bot{os.environ["BOT_TOKEN"]}'
     loop = asyncio.get_event_loop()
